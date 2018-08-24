7123f01a495284d5d69d194b0f6f16e5caaa8a54
camiloriosf <camilo.rios.f@gmail.com>
2018-08-24 12:15:47 -0300

    update addZone

--- rio/src/components/opLog/addZone.js
+++ rio/src/components/opLog/addZone.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import update from "immutability-helper";
+import classNames from "classnames";
 import * as go from "gojs";
 import { ToolManager, Diagram } from "gojs";
 import { withStyles } from "@material-ui/core/styles";
@@ -15,6 +16,29 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from "@material-ui/core/FormLabel";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import KeyboardArrowRightButton from "@material-ui/icons/KeyboardArrowRight";
+import KeyboardArrowLeftButton from "@material-ui/icons/KeyboardArrowLeft";
+import SaveIcon from "@material-ui/icons/Save";
+import RefreshIcon from "@material-ui/icons/Refresh";
 import { GojsDiagram } from "react-gojs";
 
 const styles = theme => ({
@@ -24,6 +48,7 @@ const styles = theme => ({
   content: {
     display: "flex",
     width: "100vw",
+    maxWidth: "100vw",
     height: "100vh"
   },
   flex: {
@@ -33,12 +58,41 @@ const styles = theme => ({
     width: "100%",
     flex: 1
   },
-  search: {
-    flex: 0.5
+  lines: {
+    flex: 0.8,
+    padding: 20,
+    width: "100%"
+  },
+  gridItem: {
+    padding: theme.spacing.unit
+  },
+  panels: {
+    width: "100%",
+    height: 400
   },
   formControl: {
-    margin: theme.spacing.unit,
-    minWidth: 260
+    padding: theme.spacing.unit,
+    width: "100%"
+  },
+  panelBody: {
+    margin: theme.spacing.unit
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  },
+  iconSmall: {
+    fontSize: 20
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: "100%"
   }
 });
 
@@ -77,10 +131,33 @@ class AddZone extends React.Component {
         { from: "Linea 5", to: "Barra 5" }
       ]
     },
-    zone: ""
+    zone: "",
+    lines: ["Linea 1", "Linea 2", "Linea 3"],
+    selected: [],
+    open: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
   };
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
+  };
+  handleAddLine = value => event => {
+    const { lines } = this.state;
+    const index = lines.indexOf(value);
+    lines.splice(index, 1);
+    this.setState(prev => ({ lines, selected: [...prev.selected, value] }));
+  };
+  handleRemoveLine = value => event => {
+    const { selected } = this.state;
+    const index = selected.indexOf(value);
+    selected.splice(index, 1);
+    this.setState(prev => ({ selected, lines: [...prev.lines, value] }));
   };
   createDiagram = diagramId => {
     const $ = go.GraphObject.make;
@@ -196,7 +273,7 @@ class AddZone extends React.Component {
   };
   render() {
     const { classes, open, handleClose } = this.props;
-
+    const { lines, selected } = this.state;
     return (
       <Dialog
         fullScreen
@@ -229,28 +306,202 @@ class AddZone extends React.Component {
             createDiagram={this.createDiagram}
             className={classes.diagram}
           />
-          <div className={classes.search}>
-            <FormControl className={classes.formControl}>
-              <InputLabel htmlFor="zone-simple">
-                Seleccionar Zona Predefinida
-              </InputLabel>
-              <Select
-                value={this.state.zone}
-                onChange={this.handleChange}
-                inputProps={{
-                  name: "zone",
-                  id: "zone-simple"
-                }}
+          <Grid
+            container
+            spacing={0}
+            className={classes.lines}
+            alignItems="flex-start"
+            alignContent="flex-start"
+          >
+            <Grid item xs={6}>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="zone-simple">
+                  Seleccionar Zona Predefinida
+                </InputLabel>
+                <Select
+                  value={this.state.zone}
+                  onChange={this.handleChange}
+                  inputProps={{
+                    name: "zone",
+                    id: "zone-simple"
+                  }}
+                  fullWidth
+                >
+                  <MenuItem value="">
+                    <em>Ninguna</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Zona 1</MenuItem>
+                  <MenuItem value={20}>Zona 2</MenuItem>
+                  <MenuItem value={30}>Zona 3</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              className={classes.gridItem}
+              style={{ textAlign: "right" }}
+            >
+              <Button
+                variant="contained"
+                size="small"
+                disabled
+                className={classes.button}
               >
-                <MenuItem value="">
-                  <em>Ninguna</em>
-                </MenuItem>
-                <MenuItem value={10}>Zona 1</MenuItem>
-                <MenuItem value={20}>Zona 2</MenuItem>
-                <MenuItem value={30}>Zona 3</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
+                <RefreshIcon
+                  className={classNames(classes.leftIcon, classes.iconSmall)}
+                />
+                Actualizar zona
+              </Button>
+              <Button
+                variant="contained"
+                size="small"
+                className={classes.button}
+                onClick={this.handleClickOpen}
+              >
+                <SaveIcon
+                  className={classNames(classes.leftIcon, classes.iconSmall)}
+                />
+                Guardar zona
+              </Button>
+            </Grid>
+            <Grid item xs={6} className={classes.gridItem}>
+              <Paper elevation={1} className={classes.panels}>
+                <List
+                  subheader={<ListSubheader>Lineas Disponibles</ListSubheader>}
+                >
+                  {lines.map(item => (
+                    <ListItem
+                      key={item}
+                      button
+                      onClick={this.handleAddLine(item)}
+                    >
+                      <ListItemText primary={item} />
+                      <ListItemIcon>
+                        <KeyboardArrowRightButton />
+                      </ListItemIcon>
+                    </ListItem>
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={6} className={classes.gridItem}>
+              <Paper elevation={1} className={classes.panels}>
+                <List
+                  subheader={
+                    <ListSubheader>Lineas Seleccionadas</ListSubheader>
+                  }
+                >
+                  {selected.map(item => (
+                    <ListItem
+                      key={item}
+                      button
+                      onClick={this.handleRemoveLine(item)}
+                    >
+                      <ListItemIcon>
+                        <KeyboardArrowLeftButton />
+                      </ListItemIcon>
+                      <ListItemText primary={item} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ textAlign: "center" }}
+              className={classes.gridItem}
+            >
+              <TextField
+                id="multiline-static"
+                label="Comentarios"
+                multiline
+                rows="4"
+                className={classes.textField}
+                margin="normal"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ textAlign: "left" }}
+              className={classes.gridItem}
+            >
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Tipo Desacople</FormLabel>
+                <RadioGroup
+                  aria-label="desacople"
+                  name="desacople"
+                  className={classes.group}
+                  value="economico"
+                  onChange={this.handleChange}
+                  row
+                >
+                  <FormControlLabel
+                    value="economico"
+                    control={<Radio color="primary" />}
+                    label="Economico"
+                    labelPlacement="start"
+                  />
+                  <FormControlLabel
+                    value="fisico"
+                    control={<Radio color="primary" />}
+                    label="Fisico"
+                    labelPlacement="start"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              style={{ textAlign: "center" }}
+              className={classes.gridItem}
+            >
+              <Button
+                variant="contained"
+                className={classes.button}
+                onClick={handleClose}
+              >
+                Volver
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={handleClose}
+              >
+                Crear Zona
+              </Button>
+            </Grid>
+          </Grid>
+          <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Guardar Zona</DialogTitle>
+            <DialogContent>
+              <DialogContentText>Ingresar nombre de zona</DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="zone"
+                label="Nombre Zona"
+                type="text"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancelar
+              </Button>
+              <Button onClick={this.handleClose} color="primary">
+                Guardar
+              </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </Dialog>
     );
